996b3a702c1bbb26ac2eb9d27c4b1059d4bc471a
SimNaeun <145303294+SimNaeun@users.noreply.github.com>
2023-12-27 02:12:53 +0900

    Add files via upload

---
+++ Beginner3.sql
@@ -0,0 +1,16 @@
+/*
+Group By, Order By
+*/
+
+--SELECT Gender, Age, Count(Gender) AS CountGender
+--From EmployeeDemographic
+--Where Age > 30
+--GROUP BY Gender, AGE
+--ORDER BY Gender DESC, AGE DESC
+
+--Gender 대신에 1, AGE 대신에 2로 표현가능
+--SELECT Gender, Age, Count(Gender) AS CountGender
+--From EmployeeDemographic
+--Where Age > 30
+--GROUP BY Gender, AGE
+--ORDER BY 1 DESC, 2 DESC
