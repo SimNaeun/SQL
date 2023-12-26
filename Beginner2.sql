f9bceb16dfa73af677467f9c1427b5382f8269bb
SimNaeun <145303294+SimNaeun@users.noreply.github.com>
2023-12-27 02:14:57 +0900

    Add files via upload

--- Beginner2.sql
+++ Beginner2.sql
@@ -3,43 +3,43 @@ Where Statement
 =, <>, <, >, And, Or, Like, Null, Not Null, In
 */
 
---SELECT *
---FROM EmployeeDemographic
---WHERE FirstName = 'Jim'
+SELECT *
+FROM EmployeeDemographic
+WHERE FirstName = 'Jim'
 
---SELECT *
---FROM EmployeeDemographic
---WHERE FirstName <> 'Jim'
+SELECT *
+FROM EmployeeDemographic
+WHERE FirstName <> 'Jim'
 
---SELECT *
---FROM EmployeeDemographic
---WHERE Age <= 30 AND Gender ='Male'
+SELECT *
+FROM EmployeeDemographic
+WHERE Age <= 30 AND Gender ='Male'
 
---SELECT *
---FROM EmployeeDemographic
---WHERE Age <= 30 OR Gender ='Male'
+SELECT *
+FROM EmployeeDemographic
+WHERE Age <= 30 OR Gender ='Male'
 
---SELECT *
---FROM EmployeeDemographic
---WHERE LastName Like 'S%'
-----starts only with S
+SELECT *
+FROM EmployeeDemographic
+WHERE LastName Like 'S%'
+--starts only with S
 
---SELECT *
---FROM EmployeeDemographic
---WHERE LastName Like '%S%'
-----S is anywhere
+SELECT *
+FROM EmployeeDemographic
+WHERE LastName Like '%S%'
+--S is anywhere
 
---SELECT *
---FROM EmployeeDemographic
---WHERE LastName Like 'S%o%'
---start with S and after S, o is anywhere(order)
+SELECT *
+FROM EmployeeDemographic
+WHERE LastName Like 'S%o%'
+start with S and after S, o is anywhere(order)
 
---SELECT *
---FROM EmployeeDemographic
---WHERE FirstName is NOT NULL
+SELECT *
+FROM EmployeeDemographic
+WHERE FirstName is NOT NULL
 
---SELECT *
---FROM EmployeeDemographic
---WHERE FirstName IN ('Jim', 'Michael')
---whenn including equal and multiple values
+SELECT *
+FROM EmployeeDemographic
+WHERE FirstName IN ('Jim', 'Michael')
+whenn including equal and multiple values
 
--- Beginner3.sql
+++ Beginner3.sql
@@ -2,15 +2,15 @@
 Group By, Order By
 */
 
---SELECT Gender, Age, Count(Gender) AS CountGender
---From EmployeeDemographic
---Where Age > 30
---GROUP BY Gender, AGE
---ORDER BY Gender DESC, AGE DESC
+SELECT Gender, Age, Count(Gender) AS CountGender
+From EmployeeDemographic
+Where Age > 30
+GROUP BY Gender, AGE
+ORDER BY Gender DESC, AGE DESC
 
---Gender 대신에 1, AGE 대신에 2로 표현가능
---SELECT Gender, Age, Count(Gender) AS CountGender
---From EmployeeDemographic
---Where Age > 30
---GROUP BY Gender, AGE
---ORDER BY 1 DESC, 2 DESC
+Gender 대신에 1, AGE 대신에 2로 표현가능
+SELECT Gender, Age, Count(Gender) AS CountGender
+From EmployeeDemographic
+Where Age > 30
+GROUP BY Gender, AGE
+ORDER BY 1 DESC, 2 DESC
